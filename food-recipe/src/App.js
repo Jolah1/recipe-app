1c55376f582977b6f545d5d9c89d000fc5b21cfc
okunlade Jolade <jokunlade@gmail.com>
2022-05-01 00:04:14 +0100

    Adding coun function to the project

--- food-recipe/src/App.js
+++ food-recipe/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App = () => {
+
+const APP_ID = "a0d37b32";
+const APP_KEY = "f9f60e2c85102ae58dc2a9c95767891d";
+const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+const [counter, setCounter] = useState(0);
+
+useEffect(() =>{
+console.log("Effect has been run");
+});
+
+  return(
+<div className="App">
+<form className="search-form">
+  <input className="search-bar" type="text" />
+  <button className="search-button" type="Submit">search</button>
+</form>
+<h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+</div>
   );
 }
+
+
+
 
 export default App;
